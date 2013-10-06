6690ebb4b6c9f8aecb7b3b93bbcb465fc01f90ab
Breno Moura <basmoura@gmail.com>
2013-10-05 21:26:51 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mammamia::Application.config.secret_key_base = '21c5b07ce4d71e27612611b511034d5696dc93f2fe7b59ba35d33e13e38d4caa9408a79c19c7852a0bd99d90930e9e11b0029c247d86532a44c21a1ec802ec27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mammamia::Application.config.session_store :cookie_store, key: '_mammamia_session'
