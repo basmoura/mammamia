dc2ce05c91ff08cffba11fc2467bfebadbdb5952
Breno Moura <basmoura@gmail.com>
2013-10-27 19:00:33 -0300

    cria busca

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @restaurants = Restaurant.all
-    @pizzas = Pizza.all
+    @pizzas = Pizza.paginate(page: params[:page])
+
+    if params[:name]
+      @pizzas = @pizzas.includes(:ingredients)
+      .where("LOWER(ingredients.name) = ?", params[:name].downcase).references(:ingredients)
+    end
   end
 end
