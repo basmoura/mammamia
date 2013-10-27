aaac135306289d0062ab2c1574185141409b776d
Breno Moura <basmoura@gmail.com>
2013-10-26 23:41:22 -0300

    Cria ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,53 @@
-class IngredientsController < ApplicactionController
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @ingredients = Ingredient.paginate(page: params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      redirect_to ingredients_path
+      flash["success"] = "Ingrediente cadastrado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to ingredients_path
+      flash["success"] = "Ingrediente atualizado com sucesso."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ingredient.destroy
+    redirect_to ingredients_path
+    flash["success"] = "Ingrediente removido com sucesso."
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
 end
